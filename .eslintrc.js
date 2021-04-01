93fa62c636cb4bc463b5960b0a1872a31886b510
gegorov <gegorov@yahoo.com>
2021-04-02 00:14:16 +0300

    add typecript

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,22 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+    project: './tsconfig.json'
   },
+  extends: [
+    'airbnb-typescript/base',
+  ],
+
+  plugins: [
+    '@typescript-eslint',
+  ],
   rules: {
     'no-underscore-dangle': ['error', { allow: ['__dirname', '__filename'] }],
     'import/prefer-default-export': 0,
   },
+  ignorePatterns: ["/*.*"],
 };
